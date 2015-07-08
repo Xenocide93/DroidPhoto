d8aa792f19bf33770d4efd4d59fa972b2d63af90
Froztic <rxcn6615@gmail.com>
2015-07-08 22:18:05 +0900

    settings fragment (haxx) XD

--- app/src/main/java/com/droidsans/photo/droidphoto/SettingsFragment.java
+++ app/src/main/java/com/droidsans/photo/droidphoto/SettingsFragment.java
@@ -7,25 +7,32 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.droidsans.photo.droidphoto.util.PreferenceFragment;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends PreferenceFragment {
 
 
-    public SettingsFragment() {
-        // Required empty public constructor
-    }
-
+//    public SettingsFragment() {
+//        // Required empty public constructor
+//    }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View rootView = inflater.inflate(R.layout.fragment_settings, container, false);
-        return rootView;
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.settings);
     }
+
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        // Inflate the layout for this fragment
+//        View rootView = inflater.inflate(R.layout.fragment_settings, container, false);
+//        return rootView;
+//    }
 
 
 }
