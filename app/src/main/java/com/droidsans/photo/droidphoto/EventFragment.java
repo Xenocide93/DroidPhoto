4e9648080ea521e7b173afba1d7c329b04b8b00c
ong29673 <ppong1993@hotmail.com>
2015-07-16 22:25:16 +0700

    custom option menu in event fragment

--- app/src/main/java/com/droidsans/photo/droidphoto/EventFragment.java
+++ app/src/main/java/com/droidsans/photo/droidphoto/EventFragment.java
@@ -3,12 +3,27 @@ package com.droidsans.photo.droidphoto;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class EventFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
         return inflater.inflate(R.layout.fragment_event, container, false);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        menu.clear();
+        inflater.inflate(R.menu.menu_placeholder, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
