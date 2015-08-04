07573bb48a8f5026d6dd4ecefe379b1984cb0044
Akexorcist <akexorcist@gmail.com>
2015-08-05 05:08:57 +0700

    Fix context

--- app/src/main/java/com/droidsans/photo/droidphoto/util/GlobalSocket.java
+++ app/src/main/java/com/droidsans/photo/droidphoto/util/GlobalSocket.java
@@ -142,7 +142,7 @@ public class GlobalSocket {
         try {
             if(!event.equals("user.register") && !event.equals("user.login")) {
                 Log.d("droidphoto", "set _token");
-                obj.put("_token", BaseApplication.context.getSharedPreferences(MainActivity.mContext.getString(R.string.userdata), Context.MODE_PRIVATE).getString(BaseApplication.context.getString(R.string.token), ""));
+                obj.put("_token", BaseApplication.context.getSharedPreferences(BaseApplication.context.getString(R.string.userdata), Context.MODE_PRIVATE).getString(BaseApplication.context.getString(R.string.token), ""));
             }
 //            if(!event.equals("user.register") && !event.equals("user.login")) obj.put("_token", mToken==null? getToken(): mToken);
 //            opts.query = "_token=" + ((mToken==null)? getToken(): mToken);
